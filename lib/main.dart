0fa994f4729371a09631588084f1210142b57f33
Luigi Zuccarelli <lzuccarelli@localhost.localdomain>
2020-04-27 12:29:47 +0100

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('LMZ Magic 8 Ball'),
+          backgroundColor: Colors.blue[700],
+        ),
+        body: EightBallPage(),
+      ),
+    ),
+  );
+}
+
+class EightBallPage extends StatefulWidget {
+  @override
+  _EightBallPageState createState() => _EightBallPageState();
+}
+
+class _EightBallPageState extends State<EightBallPage> {
+  int number = 1;
+
+  void rollDice() {
+    setState(() {
+      number = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rollDice();
+              },
+              child: Image.asset('images/ball$number.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
